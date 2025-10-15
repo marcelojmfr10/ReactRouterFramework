d17317e80c6650138bde54aff78518667d21973d
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-10-14 18:00:46 -0600

    Auth Layout y LoginPage

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route, prefix } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    ...prefix('/auth', [
+        layout('layouts/auth-layout.tsx', [
+            route('/login', 'routes/auth/login-page.tsx'),
+            route('/register', 'routes/auth/register-page.tsx'),
+        ])
+    ]),
+
+] satisfies RouteConfig;
